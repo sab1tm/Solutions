01ca883d8df52e9a7f3374df99d3371c275d6b21
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2022-10-03 17:19:33 +0600

    added new solution

--- src/codewars/RangeExtraction.java
+++ src/codewars/RangeExtraction.java
@@ -10,10 +10,6 @@ package codewars;
 // Solution.rangeExtraction(new int[] {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20})
 // # returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
-
 public class RangeExtraction {
 
     public static void main(String[] args) {
